48f6797a2a04f383c3f4ac69afdada476318968a
Josh Longhurst <joshlonghurst43@gmail.com>
2017-02-28 09:57:33 +0000

    Added footer and tweaked animations

--- js/script.js
+++ js/script.js
@@ -48,3 +48,19 @@ $('html, body').animate({
  $('.arrow').hover(function() {
    $(this).effect( 'bounce', { times: 3 }, 'slow');
  });
+
+
+ function onScroll(event){
+     var scrollPos = $(document).scrollTop();
+     $('#navbar a').each(function () {
+         var currLink = $(this);
+         var refElement = $(currLink.attr("href"));
+         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+             $('#menu-center ul li a').removeClass("active");
+             currLink.addClass("active");
+         }
+         else{
+             currLink.removeClass("active");
+         }
+     });
+ }
