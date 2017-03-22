4ac19d11ccdd74f69a34f90387bfa55c31af19b4
joshlonghurst <joshlonghurst43@gmail.com>
2017-03-22 13:13:26 +0000

    Added favicon support for devices and added padding to navigation elements

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#11596e">
+	<meta name="theme-color" content="#ffffff">
 	<title>Fife College Foundation</title>
 	<!-- Bootstrap Stylesheet -->
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
